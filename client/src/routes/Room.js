dd9084f5610c1bb133a2f2f08da634deaf17c0df
Cao Anh Nhất <an.dev2504@gmail.com>
2021-08-25 11:22:18 +0700

    add config

--- client/src/routes/Room.js
+++ client/src/routes/Room.js
@@ -59,6 +59,12 @@ const Room = (props) => {
       initiator: false,
       trickle: false,
       stream,
+      config: {
+        iceServers: [
+          { urls: 'stun:stun.l.google.com:19302' },
+          { urls: 'stun:global.stun.twilio.com:3478?transport=udp' },
+        ],
+      },
     });
 
     peer.on('signal', (signal) => {
@@ -83,6 +89,12 @@ const Room = (props) => {
       initiator: true,
       trickle: false,
       stream,
+      config: {
+        iceServers: [
+          { urls: 'stun:stun.l.google.com:19302' },
+          { urls: 'stun:global.stun.twilio.com:3478?transport=udp' },
+        ],
+      },
     });
 
     peer.on('signal', (signal) => {
